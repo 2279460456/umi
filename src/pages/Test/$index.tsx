6a5e76e915c3a6ad7d9d39a8afd836f523ad1361
zhaozhi01 <zhaozhi01@shizhuang-inc.com>
2023-02-13 19:13:37 +0800

    feat: 测试动态路由

---
+++ src/pages/Test/$index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RichTextEditor from './component/RichTextEditor';
+
+const Predict: React.FC = () => {
+  return <RichTextEditor></RichTextEditor>;
+};
+
+export default Predict;
